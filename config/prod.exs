7dc46e0d90ce03a89a888b35032b3b1dc6542c23
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2018-12-26 13:50:08 +0100

    enable prod secret

--- config/prod.exs
+++ config/prod.exs
@@ -73,4 +73,4 @@ config :dcbot, Witai, token: System.get_env("WITAI_TOKEN")
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-# import_config "prod.secret.exs"
+import_config "prod.secret.exs"
